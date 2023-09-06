02e130b8477fc19f9e390ca9957644fc9ea6919e
Jamil <jamil.umarmuhammad@gmail.com>
2023-09-06 01:52:46 +0000

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        <div>Product List</div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
