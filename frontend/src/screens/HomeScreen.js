dd8cf716e652b60e559658ff27aa3404ccdd4fa6
Jamil <jamil.umarmuhammad@gmail.com>
2023-09-08 04:23:20 +0000

    added axios at the frontend and updated project to get data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-import { data } from "../data";
+//import { data } from "../data";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios.get("/api/products");
+      setProducts(results.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
